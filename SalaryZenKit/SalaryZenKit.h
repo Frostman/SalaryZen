3f8d81a10e1ba12fa4565edf9ea535e59861c7a8
Sergey Lukjanov <slukjanov@mirantis.com>
2015-01-11 20:39:03 +0300

    Add empty project w/ all needed targets
    We have the following targets created in project:
    
    * iOS app
    * Shared Kit w/ tests module
    * iOS Today Ext

---
+++ SalaryZenKit/SalaryZenKit.h
@@ -0,0 +1,19 @@
+//
+//  SalaryZenKit.h
+//  SalaryZenKit
+//
+//  Created by Sergey Lukjanov on 11/01/15.
+//  Copyright (c) 2015 Sergey Lukjanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SalaryZenKit.
+FOUNDATION_EXPORT double SalaryZenKitVersionNumber;
+
+//! Project version string for SalaryZenKit.
+FOUNDATION_EXPORT const unsigned char SalaryZenKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SalaryZenKit/PublicHeader.h>
+
+
