33a4969a8b073ba0f12fbb46a28bc06650f9dd86
Sergey Lukjanov <me@slukjanov.name>
2015-01-16 03:05:30 +0300

    Current aggregator data format is going to be deprecated

--- SalaryZenAggregator/aggregate-wrapper.sh
+++ SalaryZenAggregator/aggregate-wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-flock -n /var/run/salaryzen/aggregator.lock timeout -k 2m 3m /opt/SalaryZen/SalaryZenAggregator/aggregate.sh -o /etc/public_files/salaryzen/data.json >> /var/log/salaryzen/aggregate.log 2>&1
+flock -n /var/run/salaryzen/aggregator.lock timeout -k 2m 3m /opt/SalaryZen/SalaryZenAggregator/aggregate.sh -o /etc/public_files/salaryzen/datav1.json >> /var/log/salaryzen/aggregate.log 2>&1
