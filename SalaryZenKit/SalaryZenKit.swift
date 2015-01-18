c35cae4c82e75dbdae61cc5c3d789799b63ba4db
Sergey Lukjanov <slukjanov@mirantis.com>
2015-01-18 04:12:10 +0300

    Kit updated to use aggregated data v2 with 10 min caching

--- SalaryZenKit/SalaryZenKit.swift
+++ SalaryZenKit/SalaryZenKit.swift
@@ -13,6 +13,22 @@ import SwiftyJSON
 import AlamofireSwiftyJSON
 
 
+public enum Bank: String {
+    case Alfa = "Alfa"
+    case Cbr = "CBR"
+
+    public static let all = [Alfa, Cbr]
+
+    public var title: String {
+        get { return self.rawValue }
+    }
+
+    public var name: String {
+        get { return self.rawValue.lowercaseString }
+    }
+}
+
+
 public enum Currency: String {
     case USD = "USD"
     case EUR = "EUR"
@@ -24,6 +40,41 @@ public enum Currency: String {
     public var title: String {
         get { return self.rawValue }
     }
+
+    public var name: String {
+        get { return self.rawValue.lowercaseString }
+    }
+}
+
+public enum DataType: String {
+    case Current = "Current"
+    case Historic = "Hitoric"
+
+    public static let all = [Current, Historic]
+
+    public var title: String {
+        get { return self.rawValue }
+    }
+
+    public var name: String {
+        get { return self.rawValue.lowercaseString }
+    }
+}
+
+public enum ExchangeType: String {
+    case Sell = "Sell"
+    case Buy = "Buy"
+    case Rate = "Rate"
+
+    public static let all = [Sell, Buy, Rate]
+
+    public var title: String {
+        get { return self.rawValue }
+    }
+
+    public var name: String {
+        get { return self.rawValue.lowercaseString }
+    }
 }
 
 public typealias CurrencyExchangeRate = Double
@@ -34,49 +85,130 @@ public extension CurrencyExchangeRate {
     }
 }
 
-public class CurrencyExchangeInfo {
-    private let updateHandler: (CurrencyExchangeInfo) -> Void
-    
-    public let currency: Currency
-    public var nonCashBuyRate: CurrencyExchangeRate? {
-        didSet { if oldValue != self.nonCashBuyRate { self.updateHandler(self) } }
+public class CurrencyRates {
+    private let rates = [String: Double]()
+    private let historic = [String: [NSDate: Double]]()
+
+    public var aggregatedAtTimestamp: NSTimeInterval {
+        get { return self.rates["aggregated_at"]! }
     }
-    public var nonCashSellRate: CurrencyExchangeRate? {
-        didSet { if oldValue != self.nonCashSellRate { self.updateHandler(self) } }
+
+    public init(json: JSON) {
+        for (key: String, subJson: JSON) in json.dictionaryValue {
+            if let rate = subJson.double {
+                self.rates[key] = rate
+            } else if let historic = subJson.dictionary {
+                if key.rangeOfString("historic") == nil {
+                    // error
+                }
+                if self.historic[key] == nil {
+                    self.historic[key] = [NSDate: Double]()
+                }
+                for (date: String, rateJson: JSON) in subJson {
+                    if let rate = rateJson.double {
+                        // TBD Replace NSDate() with date parsing
+                        self.historic[key]![NSDate()] = rate
+                    } else {
+                        // error
+                    }
+                }
+            } else {
+                // error!
+            }
+        }
     }
-    public var cbRate: CurrencyExchangeRate? {
-        didSet { if oldValue != self.cbRate { self.updateHandler(self) } }
+
+    public func getRate(bank: Bank, currency: Currency = Currency.USD,
+        dataType: DataType = DataType.Current, exchangeType: ExchangeType = ExchangeType.Rate)
+        -> CurrencyExchangeRate? {
+            let key = "\(bank.name)_\(currency.name)_\(dataType.name)_\(exchangeType.name)"
+            return self.rates[key]
     }
-    
-    public init(currency: Currency, updateHandler: ((info: CurrencyExchangeInfo) -> Void)? = {_ in /* NOOP */}) {
-        self.currency = currency
-        self.updateHandler = updateHandler!
+
+    public func getCurrentCbrUsdRate() -> CurrencyExchangeRate? {
+        return self.getRate(Bank.Cbr, currency: Currency.USD, dataType: DataType.Current,
+            exchangeType: ExchangeType.Rate)
+    }
+
+    public func getCurrentAlfaUsdRate(exchangeType: ExchangeType) -> CurrencyExchangeRate? {
+        return self.getRate(Bank.Alfa, currency: Currency.USD, dataType: DataType.Current,
+            exchangeType: exchangeType)
+    }
+
+    public func getCurrentAlfaUsdRates() -> (sell: CurrencyExchangeRate?,
+        buy: CurrencyExchangeRate?) {
+            return (self.getCurrentAlfaUsdRate(ExchangeType.Sell),
+                self.getCurrentAlfaUsdRate(ExchangeType.Buy))
     }
 }
 
-public class SalaryZenAggregatorDataFetcher {
-    public func updateRates(currencyExchangeInfos infos: [CurrencyExchangeInfo],
-        handler: (error: Bool) -> Void = {_ in /* NOOP */}) {
-            let url = "http://f.slukjanov.name/salaryzen/datav1.json"
-            
-            Alamofire.request(.GET, url).responseSwiftyJSON {
-                (request, response, json, error) in
-                if(error != nil) {
-                    handler(error: true)
-                } else {
-                    for info in infos {
-                        var rates = json["alfa-bank"][info.currency.title]["non-cash"]
-                        info.nonCashBuyRate = rates["buyRate"].double
-                        info.nonCashSellRate = rates["sellRate"].double
-                        
-                        rates = json["cb"][info.currency.title]["official"]
-                        info.cbRate = rates["rate"].double
-                    }
-                    
-                    handler(error: false)
+
+public class CurrencyRatesFetcher {
+    private let cache = NSCache()
+    private var cacheFor: NSTimeInterval = 10 * 60 // 10 minutes
+
+    public init() {
+        self.cache.name = "CurrencyRatesFetcherCache"
+    }
+
+    public func setConf(cacheFor: NSTimeInterval? = nil) {
+        if let cacheFor = cacheFor {
+            self.cacheFor = cacheFor
+        }
+    }
+
+    public func fetch(handler: (CurrencyRates) -> Void) {
+        let (cachedRates, outdated) = self.fetchFromCache()
+        if let cachedRates = cachedRates {
+            handler(cachedRates)
+        }
+        if outdated {
+            fetchFromServer {
+                (rates, error) in
+                // if error retry?
+                if let rates = rates {
+                    handler(rates)
                 }
             }
+        }
+    }
+
+    private func fetchFromCache() -> (rates: CurrencyRates?, outdated: Bool) {
+        if let data = self.cache.objectForKey("data") as? CurrencyRates {
+            let now = NSDate().timeIntervalSince1970
+            let outdated = now - data.aggregatedAtTimestamp < self.cacheFor
+            return (data, outdated)
+        }
+        return (nil, true)
+    }
+
+    private func fetchFromServer(handler: (rates: CurrencyRates?, error: Bool) -> Void) {
+        let url = "http://f.slukjanov.name/salaryzen/datav2.json"
+
+        Alamofire.request(.GET, url).responseSwiftyJSON {
+            (request, response, json, error) in
+            if json != nil && error == nil {
+                let rates = CurrencyRates(json: json)
+                self.cache.setObject(rates, forKey: "data")
+                handler(rates: rates, error: false)
+            } else {
+                handler(rates: nil, error: true)
+            }
+        }
     }
 }
 
-public let currencyDataFetcher = SalaryZenAggregatorDataFetcher()
+public let fetcher = CurrencyRatesFetcher()
+
+public func fetchRatesInfo(handler: (info: String) -> Void) {
+    fetcher.fetch {
+        rates in
+        let cbrRate = rates.getCurrentCbrUsdRate() ?? 0.0
+        let (alfaSellOpt, alfaBuyOpt) = rates.getCurrentAlfaUsdRates()
+        let alfaSell = alfaSellOpt ?? 0.0
+        let alfaBuy = alfaBuyOpt ?? 0.0
+        let coef = cbrRate / 36.93
+        handler(info: "$ \(alfaBuy.format()) / \(alfaSell.format())   "
+            + "cb: \(cbrRate.format()) (\(coef.format()))")
+    }
+}
--- SalaryZenKitTests/SalaryZenKitTests.swift
+++ SalaryZenKitTests/SalaryZenKitTests.swift
@@ -11,56 +11,33 @@ import XCTest
 
 import SalaryZenKit
 
+
 class SalaryZenKitTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // noop
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // noop
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
-    func testSalaryZenAggregatorDataFetcher_updateRates() {
-        let expectation = expectationWithDescription("SalaryZenAggregatorDataFetcher_updateRates")
-        var callCounter = 0
-        let handler: (info: CurrencyExchangeInfo) -> Void = {
-            (info: CurrencyExchangeInfo) in
-            callCounter++
-            return
-        }
-        
-        let infos = [CurrencyExchangeInfo(currency: Currency.USD, updateHandler: handler),
-            CurrencyExchangeInfo(currency: Currency.EUR, updateHandler: handler)]
-        
-        currencyDataFetcher.updateRates(currencyExchangeInfos: infos) {
-            (error) in
-            XCTAssertFalse(error, "There should be no error fetching data.json")
-            XCTAssertEqual(callCounter, 6, "CurrencyExchangeInfo objects should be updated 6 times")
-            
-            for info in infos {
-                XCTAssertNotNil(info.nonCashBuyRate)
-                XCTAssertNotNil(info.nonCashSellRate)
-                XCTAssertNotNil(info.cbRate)
-            }
-            
+
+    func testCurrencyRatesFetcher_fetch() {
+        let expectation = expectationWithDescription("SalaryZenKit.fetcher.fetch")
+        SalaryZenKit.fetcher.fetch {
+            (rates) in
+
+            XCTAssertNotNil(rates.getCurrentCbrUsdRate())
+
+            let (alfaSell, alfaBuy) = rates.getCurrentAlfaUsdRates()
+            XCTAssertNotNil(alfaSell)
+            XCTAssertNotNil(alfaBuy)
+
             expectation.fulfill()
         }
-        
+
         waitForExpectationsWithTimeout(10) { (error) in
             XCTAssertNil(error, "\(error)")
         }
